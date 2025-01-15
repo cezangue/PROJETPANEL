651b5dd6f0a801847bb8e5e3bf47cceed930f9a1
cezangue <cezangue@gmail.com>
2025-01-16 00:31:12 +0100

    Update page_acceuil.py

--- page_acceuil.py
+++ page_acceuil.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def set_background(opacity=0.5, color="#000000"):
+def set_background(opacity=0.5, color="#FFA500"):  # Modifiez la couleur ici pour orange
     """Définit la couleur de fond de l'application."""
     st.markdown(
         f"""
@@ -42,13 +42,13 @@ def main():
     st.set_page_config(page_title="Visualisation des Données", page_icon="")
     
     # Définir la couleur de fond
-    set_background(opacity=1.0, color="#000000")
+    set_background(opacity=1.0, color="#FFA500")  # Appliquez la couleur orange ici
 
     st.markdown("""<h1 class="animated-title">TAGNE TCHINDA VOUS SOUHAITE LA BIENVENUE DANS L'ESPACE D'ANALYSE DES EFFETS DU CHANGEMENT CLIMATIQUE EN AFRIQUE SUB-SAHARIENNE</h1>""", unsafe_allow_html=True)
     st.markdown('<h2 class="fade-in-out">Bonne navigation</h2>', unsafe_allow_html=True)
 
     # Ajouter l'image du drapeau depuis le répertoire local
-    st.image("Changement_climatique.JPG", caption="  ", use_container_width=True)  # Changement ici
+    st.image("Changement_climatique.JPG", caption="  ", use_container_width=True)
 
     st.title("Cette page, fruit du travail de TAGNE TCHINDA RINEL, nous vous proposons une vue sur la base de données utilisée pour faire des analyses, la description des différentes chroniques retenues, et l'analyse de la stationnarité des chroniques.")
     st.title("Pour voir le contenu d'une section, il vous suffit de cliquer sur le nom correspondant pour y accéder.")
