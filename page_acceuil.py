e2cb884dc3bc31b634c43fd469e1783353fdbcd7
cezangue <cezangue@gmail.com>
2025-01-16 14:40:52 +0100

    Update page_acceuil.py

--- page_acceuil.py
+++ page_acceuil.py
@@ -3,10 +3,10 @@ import streamlit as st
 # Définir la configuration de la page
 st.set_page_config(page_title="Page d'Accueil", page_icon="🏠")
 
-# Définir le titre de l'application
-#st.markdown("<h1 style='text-align: center;'>Page d'Accueil: TAGNE TCHINDA vous souhaite la bienvenue dans l'espace d'analyse des effets du changement climatique en Afrique sub-saharienne</h1>", unsafe_allow_html=True)
+# Définir le titre de l'application avec effet de défilement
 st.markdown("<div class='scroll-text'><h1>Page d'Accueil: TAGNE TCHINDA vous souhaite la bienvenue dans l'espace d'analyse des effets du changement climatique en Afrique sub-saharienne</h1></div>", unsafe_allow_html=True)
 st.markdown('<h2>Bonne navigation</h2>', unsafe_allow_html=True)
+
 # Fonction pour créer des onglets
 def display_tabs():
     tabs = ["Volet de visualisation des indicateurs", "Analyse des stationnarité des séries", "Modélisation en Panel"]
@@ -48,17 +48,31 @@ def display_visualisation():
         }
         h2 {
             color: #ADD8E6;
+            text-align: center;
+        }
+        .image-container {
+            float: left;
+            width: 30%; /* Largeur du cadre de l'image */
+            padding: 10px;
+            border: 2px solid #000; /* Bordure noire */
+            border-radius: 10px; /* Coins arrondis */
+            margin-right: 10px; /* Marge à droite */
         }
         </style>
         """,
         unsafe_allow_html=True,
     )
 
-    # Affichage de l'image pour le dépannage
-    st.image("pages de navigation/Changement_climatique.JPG", caption="Changement Climatique", use_container_width=True)
     # Contenu principal
-    st.markdown("<h2 style='text-align: center;'>Nous sommes disponibles vie l'adresse mail: cezangue@gmail.com</h2>", unsafe_allow_html=True)
-     #st.markdown("<div class='main'>", unsafe_allow_html=True)
+    st.markdown("<div class='main'>", unsafe_allow_html=True)
+    
+    # Affichage de l'image dans un cadre
+    st.markdown("<div class='image-container'>", unsafe_allow_html=True)
+    st.image("pages de navigation/Changement_climatique.JPG", caption="Changement Climatique", use_column_width=True)
+    st.markdown("</div>", unsafe_allow_html=True)
+
+    # Autres contenus
+    st.markdown("<h2>Nous sommes disponibles via l'adresse mail: cezangue@gmail.com</h2>", unsafe_allow_html=True)
     st.write("Cette page, fruit du travail de TAGNE TCHINDA RINEL, nous vous proposons une vue sur la base de données utilisée pour faire des analyses, la description des différentes chroniques retenues, et l'analyse de la stationnarité des chroniques.")
     st.write("Pour voir le contenu d'une section, il vous suffit de cliquer sur le nom correspondant pour y accéder.")
     st.markdown("</div>", unsafe_allow_html=True)
