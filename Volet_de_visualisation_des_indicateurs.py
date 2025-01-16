bbcc1c11d597205e69eaa7154f71e21030ca3f38
cezangue <cezangue@gmail.com>
2025-01-16 16:08:59 +0100

    Update Volet_de_visualisation_des_indicateurs.py

--- Volet_de_visualisation_des_indicateurs.py
+++ Volet_de_visualisation_des_indicateurs.py
@@ -2,8 +2,11 @@ import streamlit as st
 import plotly.graph_objects as go
 
 def display():
+    st.title("Bienvenue sur la page de visualisation")  # Titre de la nouvelle page
     st.subheader("Volet de visualisation des indicateurs")
-    # Exemple de création d'un graphique avec Plotly
+    
+    # Exemple de graphique
     fig = go.Figure(data=go.Scatter(y=[2, 3, 1], mode='lines+markers'))
     st.plotly_chart(fig)
+    
     st.write("Contenu de la visualisation des indicateurs.")
