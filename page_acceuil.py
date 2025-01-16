e652792933dda333fa37aede0f54cc563690a5ac
cezangue <cezangue@gmail.com>
2025-01-16 11:53:36 +0100

    Update page_acceuil.py

--- page_acceuil.py
+++ page_acceuil.py
@@ -21,8 +21,8 @@ def display_visualisation():
         """
         <style>
         body {
-            background-color: #FFA500; /* Couleur de fond orange */
-            color: white;
+            background-color: #FFFF00; /* Couleur de fond jaune */
+            color: black; /* Couleur du texte noire pour contraste */
             font-family: Arial, sans-serif;
         }
         h1 {
@@ -55,7 +55,7 @@ def display_visualisation():
     st.markdown('<h2>Bonne navigation</h2>', unsafe_allow_html=True)
 
     # Ajouter l'image du drapeau depuis le répertoire local
-    st.image("pages de navigation/Changement_climatique.JPG", caption="  ", use_container_width=True)  # Assurez-vous que le chemin est correct
+    st.image("pages de navigation/Changement_climatique.JPG", caption="Changement Climatique", use_container_width=True)  # Assurez-vous que le chemin est correct
 
     st.write("Cette page, fruit du travail de TAGNE TCHINDA RINEL, nous vous proposons une vue sur la base de données utilisée pour faire des analyses, la description des différentes chroniques retenues, et l'analyse de la stationnarité des chroniques.")
     st.write("Pour voir le contenu d'une section, il vous suffit de cliquer sur le nom correspondant pour y accéder.")
